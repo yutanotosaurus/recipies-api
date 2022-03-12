88ca33f3ef30b8cedcf1b7e8b484d2da55398184
yutanotosaurus <88889234+yutanotosaurus@users.noreply.github.com>
2022-03-12 22:39:24 +0900

    new recipe endpoint

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/rs/xid v1.3.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220307211146-efcb8507fb70 // indirect
 	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 type Recipe struct {
-	Name        string    `json:"name"`
-	Tags        string    `json:"tags"`
-	Ingredients []string  `json:ingredients`
-	Instruction []string  `json:instructions`
-	PublishedAt time.Time `json:"publishedAt"`
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:ingredients`
+	Instructions []string  `json:instructions`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+func NewRecipeHendler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipes)
 }
 
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHendler)
 	router.Run()
 }
